18ec6115f5f07ed32b1fe1c7595877ab22e831f7
Brad <brthomiu@gmail.com>
2022-09-13 15:08:54 -0700

    Add header components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/portraitCrop.png'
+import HeaderSocials from './HeaderSocials'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+     
+        <h5>Hello I'm</h5>
+        <h1>Brad Thompson</h1>
+        <h5 className="text-light">Software, Data, & Operations Specialist</h5>
+      
+        <CTA />
+
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="Me" />
+        </div>
+
+      <a href="#contact" className="scroll__down">Scroll Down</a>
+
+
+      </div>
+    </header>
   )
 }
 
